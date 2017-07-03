8ce441f3b2aa1786e8aa326ab043cec8890e751d
Dobromir Zlatev <dobromir@soundmouse.com>
2017-07-03 16:56:45 +0100

    asd

--- Refresh/Program.cs
+++ Refresh/Program.cs
@@ -2,6 +2,11 @@
 using System.IO;
 using Microsoft.VisualBasic.FileIO;
 using System.Collections.Generic;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Auth.OAuth2;
+using System.Threading;
+using Google.Apis.Calendar.v3.Data;
 
 namespace Refresh
 {
@@ -14,10 +19,54 @@ namespace Refresh
         };
         static void Main(string[] args)
         {
-            DailyFolders df = new DailyFolders(@"C:\Users\dobromir\Desktop\", 
-                @"F:\WorkBackup\", 
-                new List<string>() { @"C:\Users\dobromir\Downloads\", @"C:\Users\dobromir\Pictures\" }, 
+            DailyFolders df = new DailyFolders(@"C:\Users\dobromir\Desktop\",
+                @"F:\WorkBackup\",
+                new List<string>() { @"C:\Users\dobromir\Downloads\", @"C:\Users\dobromir\Pictures\" },
                 true);
+
+            //try
+            //{
+            //    UserCredential credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            //    new ClientSecrets
+            //    {
+            //        ClientId = "241811363389-gkb4q30v85brccom3sur5spaap24scm4.apps.googleusercontent.com",
+            //        ClientSecret = "Kn9cWTMBTyFr7u7aQ6dRAALB",
+            //    },
+            //    new[] { CalendarService.Scope.Calendar },
+            //    "DobromirZlatev",
+            //    CancellationToken.None).Result;
+            //    var service = new CalendarService(new BaseClientService.Initializer()
+            //    {
+            //        HttpClientInitializer = credential,
+            //        ApplicationName = "CalendarQuickstart",
+            //    });
+            //    Event myEvent = new Event
+            //    {
+            //        Summary = "Appointment",
+            //        Location = "Somewhere",
+            //        Start = new EventDateTime()
+            //        {
+            //            DateTime = DateTime.Now.AddHours(1),
+            //            TimeZone = "Europe/London"
+            //        },
+            //        End = new EventDateTime()
+            //        {
+            //            DateTime = DateTime.Now.AddHours(2),
+            //            TimeZone = "Europe/London"
+            //        },
+            //        Recurrence = new String[] { "RRULE:FREQ=WEEKLY;BYDAY=MO" },
+            //        Attendees = new List<EventAttendee>()
+            //        {
+            //            new EventAttendee() { Email = "dobromirzlatessv@gmail.com" }
+            //        }
+            //    };
+
+            //    Event recurringEvent = service.Events.Insert(myEvent, "primary").Execute();
+            //}
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //}
 
             try
             {
@@ -32,9 +81,9 @@ namespace Refresh
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("Archiving...");
                 df.ArchiveFolders();
-                
+
                 Console.WriteLine("Archive Complete. (" + df.ArchiveChanges + " " + df.ChangeOrChanges(df.ArchiveChanges) + ")\n");
-                
+
             }
             catch (Exception ex)
             {
